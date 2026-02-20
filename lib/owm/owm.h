9d64f1d2f0297f330b8ae8dcbc5ef0acf41ebb71
Mats Karlsson <68421483+mats-nk@users.noreply.github.com>
2026-02-20 16:54:38 +0100

    Remove unused includes from owm.h
    Removed unnecessary includes for ArduinoJson and httpsRequestJson.
    
    Signed-off-by: Mats Karlsson <68421483+mats-nk@users.noreply.github.com>

--- lib/owm/owm.h
+++ lib/owm/owm.h
@@ -2,8 +2,6 @@
 #define WLEDPIXEL_OWM_H
 
 #include <Arduino.h>
-#include <ArduinoJson.h>
-#include "httpsRequestJson.h"
 
 void owmWeatherUpdate(String city, String unitsFormat, String token);
 String openWetherMapGetWeather(String whatToDisplay, String unitsFormat);
