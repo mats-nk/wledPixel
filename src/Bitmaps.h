cd5c20901132d3e9f8f05c860b2fb420f15f92d7
Mats Karlsson <68421483+mats-nk@users.noreply.github.com>
2026-02-24 10:13:51 +0100

    Refactor bitmap definitions and remove duplicates
    Signed-off-by: Mats Karlsson <68421483+mats-nk@users.noreply.github.com>

--- src/Bitmaps.h
+++ src/Bitmaps.h
@@ -16,56 +16,6 @@
   #define FLASH_BITMAP
 #endif
 
-// ─── Pacman
-// ──────────────────────────────────────────────────────────────────────
-#define F_PMAN1   1
-#define W_PMAN1   8
-extern const uint8_t pacman1[] PROGMEM FLASH_BITMAP;
-
-#define F_PMAN2   1
-#define W_PMAN2   8
-extern const uint8_t pacman2[] PROGMEM FLASH_BITMAP;
-
-// ─── Wave
-// ──────────────────────────────────────────────────────────────────────
-#define F_WAVE    1
-#define W_WAVE    8
-extern const uint8_t wave[] PROGMEM FLASH_BITMAP;
-
-// ─── Mario
-// ─────────────────────────────────────────────────────────────────────
-#define F_MARIO   1
-#define W_MARIO   8
-extern const uint8_t mario[] PROGMEM FLASH_BITMAP;
-
-// ─── Ghost
-// ─────────────────────────────────────────────────────────────────────
-#define F_GHOST   1
-#define W_GHOST   8
-extern const uint8_t ghost[] PROGMEM FLASH_BITMAP;
-
-// ─── Dino
-// ──────────────────────────────────────────────────────────────────────
-#define F_DINO    1
-#define W_DINO    8
-extern const uint8_t dino[] PROGMEM FLASH_BITMAP;
-
-// ─── Roll
-// ──────────────────────────────────────────────────────────────────────
-#define F_ROLL1   1
-#define W_ROLL1   8
-extern const uint8_t roll1[] PROGMEM FLASH_BITMAP;
-
-#define F_ROLL2   1
-#define W_ROLL2   8
-extern const uint8_t roll2[] PROGMEM FLASH_BITMAP;
-
-// ─── Lines
-// ─────────────────────────────────────────────────────────────────────
-#define F_LINES   1
-#define W_LINES   8
-extern const uint8_t lines[] PROGMEM FLASH_BITMAP;
-
 // ─── Arrow
 // ─────────────────────────────────────────────────────────────────────
 #define F_ARROW1  1
@@ -76,17 +26,29 @@ extern const uint8_t arrow1[] PROGMEM FLASH_BITMAP;
 #define W_ARROW2  8
 extern const uint8_t arrow2[] PROGMEM FLASH_BITMAP;
 
-// ─── Sailboat
-// ──────────────────────────────────────────────────────────────────
-#define F_SAILBOAT 1
-#define W_SAILBOAT 8
-extern const uint8_t sailboat[] PROGMEM FLASH_BITMAP;
+// ─── Chevron
+// ───────────────────────────────────────────────────────────────────
+#define F_CHEVRON 1
+#define W_CHEVRON 8
+extern const uint8_t chevron[] PROGMEM FLASH_BITMAP;
 
-// ─── Steamboat
-// ─────────────────────────────────────────────────────────────────
-#define F_STEAMBOAT 1
-#define W_STEAMBOAT 8
-extern const uint8_t steamboat[] PROGMEM FLASH_BITMAP;
+// ─── Dino
+// ──────────────────────────────────────────────────────────────────────
+#define F_DINO    1
+#define W_DINO    8
+extern const uint8_t dino[] PROGMEM FLASH_BITMAP;
+
+// ─── Fireball
+// ──────────────────────────────────────────────────────────────────
+#define F_FBALL   1
+#define W_FBALL   8
+extern const uint8_t fireball[] PROGMEM FLASH_BITMAP;
+
+// ─── Ghost
+// ─────────────────────────────────────────────────────────────────────
+#define F_GHOST   1
+#define W_GHOST   8
+extern const uint8_t ghost[] PROGMEM FLASH_BITMAP;
 
 // ─── Heart
 // ─────────────────────────────────────────────────────────────────────
@@ -100,26 +62,64 @@ extern const uint8_t heart[] PROGMEM FLASH_BITMAP;
 #define W_INVADER 8
 extern const uint8_t invader[] PROGMEM FLASH_BITMAP;
 
+// ─── Lines
+// ─────────────────────────────────────────────────────────────────────
+#define F_LINES   1
+#define W_LINES   8
+extern const uint8_t lines[] PROGMEM FLASH_BITMAP;
+
+// ─── Mario
+// ─────────────────────────────────────────────────────────────────────
+#define F_MARIO   1
+#define W_MARIO   8
+extern const uint8_t mario[] PROGMEM FLASH_BITMAP;
+
+// ─── Pacman
+// ──────────────────────────────────────────────────────────────────────
+#define F_PMAN1   1
+#define W_PMAN1   8
+extern const uint8_t pacman1[] PROGMEM FLASH_BITMAP;
+
+#define F_PMAN2   1
+#define W_PMAN2   8
+extern const uint8_t pacman2[] PROGMEM FLASH_BITMAP;
+
 // ─── Rocket
 // ────────────────────────────────────────────────────────────────────
 #define F_ROCKET  1
 #define W_ROCKET  8
 extern const uint8_t rocket[] PROGMEM FLASH_BITMAP;
 
-// ─── Fireball
-// ──────────────────────────────────────────────────────────────────
-#define F_FBALL   1
-#define W_FBALL   8
-extern const uint8_t fireball[] PROGMEM FLASH_BITMAP;
+// ─── Roll
+// ──────────────────────────────────────────────────────────────────────
+#define F_ROLL1   1
+#define W_ROLL1   8
+extern const uint8_t roll1[] PROGMEM FLASH_BITMAP;
 
-// ─── Chevron
-// ───────────────────────────────────────────────────────────────────
-#define F_CHEVRON 1
-#define W_CHEVRON 8
-extern const uint8_t chevron[] PROGMEM FLASH_BITMAP;
+#define F_ROLL2   1
+#define W_ROLL2   8
+extern const uint8_t roll2[] PROGMEM FLASH_BITMAP;
+
+// ─── Sailboat
+// ──────────────────────────────────────────────────────────────────
+#define F_SAILBOAT 1
+#define W_SAILBOAT 8
+extern const uint8_t sailboat[] PROGMEM FLASH_BITMAP;
+
+// ─── Steamboat
+// ─────────────────────────────────────────────────────────────────
+#define F_STEAMBOAT 1
+#define W_STEAMBOAT 8
+extern const uint8_t steamboat[] PROGMEM FLASH_BITMAP;
 
 // ─── Walker
 // ────────────────────────────────────────────────────────────────────
 #define F_WALKER  1
 #define W_WALKER  8
 extern const uint8_t walker[] PROGMEM FLASH_BITMAP;
+
+// ─── Wave
+// ──────────────────────────────────────────────────────────────────────
+#define F_WAVE    1
+#define W_WAVE    8
+extern const uint8_t wave[] PROGMEM FLASH_BITMAP;
